92b9fd62453b48542846bcec01ae48fbd3cbd647
Saul A. Chacon G <japones75@gmail.com>
2014-07-27 13:33:49 -0430

    Creado el proyecto y agregado status al mismo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DawBook::Application.config.secret_key_base = '0076e993746034812390052855949318085ae3db739f25fe813588eef0f49c1cee94470a13631f31d51238e85d2450de41d84ef56f93e3c0db463081a9a4616e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DawBook::Application.config.session_store :cookie_store, key: '_DawBook_session'
